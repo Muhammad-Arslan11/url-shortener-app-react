17e5d96e98ccee785f1c165b7a8cf1a78c628d1c
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2025-06-04 22:10:59 +0500

    Login component almost complete

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,8 +10,24 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { BeatLoader } from "react-spinners";
+import Error from './Error'
 
 function Login() {
+  const [formData, setFormData]  = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleInputChange = (e)=>{
+    e.preventDefault();
+    const [name, value] = e.target;
+    setFormData((prevData)=> ({
+      ...prevData,
+      [name] : value,
+    }))
+
+
+  }
   return (
     <div>
       <Card className="bg-gray-800 border-2 border-black">
@@ -20,19 +36,28 @@ function Login() {
           <CardDescription>Login to your account</CardDescription>
           {/* <CardAction>Card Action</CardAction> */}
         </CardHeader>
+         <Error message="An error occurred"/>
         <CardContent className="space-y-2">
           <div className="space-y-1">
-            <Input name="email" type="email" placeholder="Enter your email" />
+            <Input 
+            name="email" 
+            type="email" 
+            placeholder="Enter your email" 
+            onClick={handleInputChange}
+            />
+            <Error message="An error occurred"/>
             <Input
               name="password"
               type="password"
               placeholder="Enter your password"
+              onClick={handleInputChange}
             />
+            <Error message="An error occurred"/>
           </div>
         </CardContent>
         <div className="flex justify-center">
           <CardFooter>
-            <Button>
+            <Button onClick={handleLogin}>
               {true ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
             </Button>
           </CardFooter>
